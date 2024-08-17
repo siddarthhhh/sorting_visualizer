1bfdbb5e0b6c1f41a4ef3426b23f11e00ca4fc23
siddarthhhh <chidurulasiddarth@gmail.com>
2024-08-18 04:04:51 +0530

    added sound effects

--- script.js
+++ script.js
@@ -1,46 +1,101 @@
-const n=10;
-const arr=[];
+const n = 20;
+const arr = [];
 
 init();
 
+let audioctx = null;
 
-function init(){
-    for(let i=0;i<n;i++){
-        arr[i]=Math.random();
+function playnote(freq) {
+    if (audioctx === null) {
+        audioctx = new (
+            AudioContext ||
+            webkitAudioContext ||
+            window.webkitAudioContext
+        )();
+    }
+
+    const dur = 0.1;
+    const osc = audioctx.createOscillator();
+    const gainNode = audioctx.createGain();
+    
+    osc.frequency.value = freq;
+    osc.connect(gainNode);
+    gainNode.gain.value = 0.1;
+    
+    gainNode.connect(audioctx.destination);
+    
+    osc.start();
+    osc.stop(audioctx.currentTime + dur);
+}
+
+function init() {
+    for (let i = 0; i < n; i++) {
+        arr[i] = Math.random();
     }
     showbars();
 }
 
-function play(){
-    bubblesort(arr);
-    showbars;
+function play() {
+    const copy = [...arr];
+    const moves = bubblesort(copy);
+    animate(moves);
 }
 
+function animate(moves) {
+    if (moves.length === 0) {
+        showbars();
+        return;
+    }
 
-function bubblesort(arr){
-    
-    do{
-        var swapped=false;
+    const move = moves.shift();
+    const [i, j] = move.indices;
 
-        for(let i=1;i<arr.length;i++){
-            if(arr[i-1]>arr[i]){
-                swapped=true;
-                [arr[i-1],arr[i]]=[arr[i],arr[i-1]];
+    if (move.type === "swap") {
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
 
+    playnote(200 + arr[i] * 500);
+    playnote(200 + arr[j] * 500);
+    showbars(move);
+
+    setTimeout(() => {
+        animate(moves);
+    }, 200);
+}
+
+function bubblesort(arr) {
+    const moves = [];
+    do {
+        var swapped = false;
+
+        for (let i = 1; i < arr.length; i++) {
+            moves.push({
+                indices: [i - 1, i], type: "comp"
+            });
+            if (arr[i - 1] > arr[i]) {
+                swapped = true;
+                moves.push({
+                    indices: [i - 1, i], type: "swap"
+                });
+                [arr[i - 1], arr[i]] = [arr[i], arr[i - 1]];
             }
         }
-    }while(swapped);
+    } while (swapped);
 
-
+    return moves;
 }
 
-function showbars(){
-    container.innerHTML="";
-    for(let i=0;i<arr.length;i++){
-        const bar=document.createElement("div");
-        bar.style.height=arr[i]*100+"%";
+function showbars(move) {
+    const container = document.getElementById('container');
+    container.innerHTML = "";
+    for (let i = 0; i < arr.length; i++) {
+        const bar = document.createElement("div");
+        bar.style.height = arr[i] * 100 + "%";
         bar.classList.add("bar");
+
+        if (move && move.indices.includes(i)) {
+            bar.style.backgroundColor = move.type === "swap" ? "red" : "blue";
+        }
         container.appendChild(bar);
     }
-    
 }
