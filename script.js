2f0ec9ec2e56a47cb865cf0357eda366dae0f58d
siddarthhhh <chidurulasiddarth@gmail.com>
2024-08-18 03:20:34 +0530

    added visible dimensions,functions,buttons

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
 const n=10;
 const arr=[];
 
-for(let i=0;i<n;i++){
-    arr[i]=Math.random();
+init();
+
+
+function init(){
+    for(let i=0;i<n;i++){
+        arr[i]=Math.random();
+    }
+    showbars();
 }
 
-for(let i=0;i<arr.length;i++){
-    const bar=document.createElement("div");
-    bar.style.height=arr[i]*100+"%";
-    bar.style.width="10px";
-    bar.style.backgroundColor="black";
-    container.appendChild(bar);
+function play(){
+    bubblesort(arr);
+    showbars;
+}
+
+
+function bubblesort(arr){
+    
+    do{
+        var swapped=false;
+
+        for(let i=1;i<arr.length;i++){
+            if(arr[i-1]>arr[i]){
+                swapped=true;
+                [arr[i-1],arr[i]]=[arr[i],arr[i-1]];
+
+            }
+        }
+    }while(swapped);
+
+
+}
+
+function showbars(){
+    container.innerHTML="";
+    for(let i=0;i<arr.length;i++){
+        const bar=document.createElement("div");
+        bar.style.height=arr[i]*100+"%";
+        bar.classList.add("bar");
+        container.appendChild(bar);
+    }
+    
 }
